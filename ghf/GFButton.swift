a2c234d601e35a2d3212c9424cbae3b2a90dce55
Marcelo Toscano <toscanocombr@gmail.com>
2021-02-16 15:33:29 -0300

    custom views - button and textfield

---
+++ ghf/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  ghf
+//
+//  Created by toscano on 2/16/21.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(bgColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = bgColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure(){
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
